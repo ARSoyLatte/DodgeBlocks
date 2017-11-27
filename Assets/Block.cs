bcd1add8c3b48fc728cc32efce45c74cb6344f03
Paul Kohler <pkohler48@gmail.com>
2017-11-26 23:24:09 -0500

    Completed Basic Dodge Game

---
+++ Assets/Block.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour {
+
+    public float gravityScaleModifier = 20f;
+
+    private void Start()
+    {
+        GetComponent<Rigidbody2D>().gravityScale = 1f+Time.timeSinceLevelLoad / gravityScaleModifier;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (transform.position.y < -2f){
+            Destroy(gameObject);
+        }
+	}
+}
--- Assets/Player.cs
+++ Assets/Player.cs
@@ -4,13 +4,31 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public float speed = 15f;
+    public float gameWidth = 5f;
+
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Use Fixed Update to make changes on a fixed interval
+    private void FixedUpdate()
+    {
+        //Get horizontal input and adjust the -1 to 1 value based on the delta
+        float x = Input.GetAxis("Horizontal") * Time.fixedDeltaTime * speed;
+
+        Vector2 newPosition = rb.position + Vector2.right * x;
+
+        newPosition.x = Mathf.Clamp(newPosition.x, -gameWidth, gameWidth);
+
+        rb.MovePosition(newPosition);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        FindObjectOfType<GameManager>().EndGame();
+    }
 }
